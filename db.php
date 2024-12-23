6ab2b5e8ffe49644b99da22a180bdbc39d9899f5
J.H.K <83626074+wogho@users.noreply.github.com>
2024-12-23 23:18:19 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,24 @@
 <?php
-$servername = "129.154.215.197";
-$username = "root";
-$password = "Wldhr@12";
-$dbname = "bulletin_board";
+require_once __DIR__ . '/vendor/autoload.php'; // Composer autoload 로드
+
+use Dotenv\Dotenv;
+
+// .env 파일 로드
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// 환경 변수에서 값 읽기
+$servername = $_ENV['DB_SERVER'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
 
 // 데이터베이스 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 연결 확인
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("Connection failed: " . $conn->connect_error);
+    die("데이터베이스 연결 실패. 관리자에게 문의하세요.");
 }
 ?>
